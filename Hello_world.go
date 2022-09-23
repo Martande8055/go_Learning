ce8fe99344baa71392426b8150c1ffead1bf3ab4
Martande8055 <saurabh9420448558@gmail.com>
2022-09-23 22:31:28 +0530

    1st code done

--- Hello_world.go
+++ Hello_world.go
@@ -1,7 +1,9 @@
-package main
+package main /*The package “main” tells the Go compiler that the package
+  should compile as an executable program instead of a shared library.*/
 
-import "fmt"
+import "fmt" /*fmt stands for the Format package.This package is all about formatting input and output.
+it is a preprocessor command which tells the compiler to include the files lying in the package.*/
 
-func main() {
+func main() { //The main function in the package “main” will be the entry point of our executable program.
 	fmt.Println("hello world")
 }
